994200ff3bd1022bdc422e374617e677163510a2
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-10-19 13:34:52 +0300

    observer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { store } from "./store";
+import { useStore } from "./useStore";
+
+const Text = () => {
+  const count = useStore(-1);
+
+  return <h1>{count}</h1>;
+};
 
 function App() {
   return (
     <div className="App">
+      <button onClick={(e) => store.increment()}>Increment the count</button>
       <header className="App-header">
+        <Text />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
